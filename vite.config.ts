94ce5c58863f4a71636b627b7ca4e5d7d069b6c5
Steven <stevenibz-96@hotmail.com>
2026-04-01 02:15:36 +0200

    Configuracion de alias

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,20 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@views': fileURLToPath(new URL('./src/views', import.meta.url)),
+      '@stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
+      '@data': fileURLToPath(new URL('./data', import.meta.url)),
+      '@composables': fileURLToPath(new URL('./src/composables', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@common': fileURLToPath(new URL('./src/components/common', import.meta.url)),
+      '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
+      '@router': fileURLToPath(new URL('./src/router', import.meta.url)),
+      '@services': fileURLToPath(new URL('./src/services', import.meta.url)),
+      '@icon': fileURLToPath(new URL('./src/assets', import.meta.url)),
     },
   },
 })
